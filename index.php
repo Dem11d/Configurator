f2ee32686643906b7c8342b447e9adc26d491eb3
Demid Pavliuk <dem11d92@gmail.com>
2017-01-16 12:14:49 +0200

    original version

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+include "controllers/admin/AdminCaseBackgroundController.php";
+$obj = new AdminCaseBackgroundController();
+$params = $obj->initContent();
+
+
+
+$params['front_image'] = 'img/customImage/android/'.$params['device_android'][0]['android'];
+$params['back_image'] = 'products/back.png';
+/* $params['fonts'] = 'products/back.png'; */
+include "modules/templ.php";
+
+?>
